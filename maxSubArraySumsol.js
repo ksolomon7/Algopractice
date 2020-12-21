547ee554d705f910d20580be8f6fbb81ac94a457
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-12-21 13:29:06 -0500

    new section

---
+++ maxSubArraySumsol.js
@@ -0,0 +1,13 @@
+//write a function called maxSubArraySum which
+//accepts an array of integers and a number called n. 
+//function should calculate the maximum sum of consecutive
+//elements
+
+function maxSubArraySum(arr,num){
+    let temp=0;
+    let max=0;
+
+    if(arr.length<num) return null;
+
+    for(let)
+}
